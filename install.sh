66102062eb5ac19a8a3e18f5f001faffe0c2290a
Ambroise Maupate <ambroise@rezo-zero.com>
2017-07-03 16:55:51 +0200

    Added latest install scripts for docker and docker-compose

--- install.sh
+++ install.sh
@@ -1,7 +1,42 @@
 #!/bin/bash
 #
 apt-get update;
-apt-get install ntp ntpdate nano htop curl curlftpfs zsh fail2ban postfix mailutils;
+apt-get install \
+    ntp \
+    ntpdate \
+    nano \
+    htop \
+    curl \
+    curlftpfs \
+    zsh \
+    fail2ban \
+    postfix \
+    mailutils \
+    apt-transport-https \
+    ca-certificates \
+    software-properties-common;
+
+# Install latest docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -;
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable";
+
+apt-get update;
+apt-get install docker-ce;
+
+# Add your user to docker group
+# for non-root installs
+# usermod -aG docker $USER;
+
+# Configure Docker to start on boot
+# with systemd
+systemctl enable docker;
+
+# Install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose;
+chmod +x /usr/local/bin/docker-compose;
 
 #
 # Listen only localhost for Postfix
