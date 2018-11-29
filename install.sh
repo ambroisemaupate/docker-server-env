cc0d910c9dc646908dd99b4d85620d3a6627260c
Ambroise Maupate <ambroise@rezo-zero.com>
2018-11-29 13:55:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -45,8 +45,8 @@ groupadd docker;
 systemctl enable docker;
 
 # Install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose;
-chmod +x /usr/local/bin/docker-compose;
+curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 #
 # Listen only localhost for Postfix
