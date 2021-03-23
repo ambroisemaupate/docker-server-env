3ca5d9d21a32b27beb51e6740a546b937ecba992
Ambroise Maupate <ambroise@rezo-zero.com>
2021-03-23 16:53:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ groupadd docker;
 systemctl enable docker;
 
 # Install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.28.6/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 #
